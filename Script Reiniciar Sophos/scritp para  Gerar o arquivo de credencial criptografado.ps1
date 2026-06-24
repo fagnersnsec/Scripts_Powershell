8f5da3590314835a5b4fb843edfc8f6fba30e960
fagner <fagner@novatera.com.br>
2026-06-24 15:59:20 -0300

    Script para Reiciar o Sophos Atrves de Agendador de Tarefa do Windows

--- Script Reiniciar Sophos/scritp para  Gerar o arquivo de credencial criptografado.ps1
+++ Script Reiniciar Sophos/scritp para  Gerar o arquivo de credencial criptografado.ps1
@@ -1,2 +1,4 @@
-﻿$cred = Get-Credential -UserName "admin" -Message "abc@123"
+﻿
+#script para a credencial criptografa do sophos.
+$cred = Get-Credential -UserName "admin" -Message "abc@123"
 $cred | Export-Clixml -Path "D:\logs\sophos_cred.xml"
